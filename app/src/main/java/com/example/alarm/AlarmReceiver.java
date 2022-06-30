8bb7c5af6ede9eb5e2e540115f4f05167de5b467
ryuzakizx <gcmaxom@gmail.com>
2022-06-30 09:22:14 +0700

    Project Java Android Studio Alarm

---
+++ app/src/main/java/com/example/alarm/AlarmReceiver.java
@@ -0,0 +1,19 @@
+package com.example.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    MediaPlayer player;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm aktif @kamaludin!",
+                Toast.LENGTH_LONG).show();
+        player = MediaPlayer.create(context, R.raw.soundkamal);
+        player.start();
+    }
+}
